d9d694a88f3df0d2aa3ffa5ad527a5d9d3348df4
Jalkiran <jalkirankaur36@gmail.com>
2026-06-27 16:52:14 +0530

    Changed the data type of numero from string to float

--- cleaning.py
+++ cleaning.py
@@ -3,3 +3,7 @@ df=pd.read_csv(r"C:\Users\Acer\Downloads\amazon_fires.csv",encoding='latin1')
 
 #Average of fires of each state
 print(df["numero"].dtype)
+
+df["numero"] = df["numero"].str.replace(".", "", regex=False)
+df["numero"] = pd.to_numeric(df["numero"], errors='coerce')
+print(df["numero"].dtype)
